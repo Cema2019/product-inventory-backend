0e5e2d3686b230092a425b11ae21502d65cfa7c5
Carlos M <53409854+Cema2019@users.noreply.github.com>
2025-08-15 16:46:50 -0600

    Update Cors

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ app = FastAPI(title="Sales Inventory API - FastAPI Version")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  
+    allow_origins=["http://localhost:5173"],  
     allow_credentials=True,
     allow_methods=["*"],  # GET, POST, PUT, DELETE
     allow_headers=["*"],
